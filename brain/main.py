2086c8fe1651b86c2551ce115149cfdfd222dcb5
Gonzalo933 <gonzalo.hernandez.1293@gmail.com>
2017-07-16 01:10:58 +0200

    Now parses messages received

--- brain/main.py
+++ brain/main.py
@@ -1,18 +1,22 @@
-import actions.countdown
+import brain.actions.countdown
 import re
 
 def parse_command(command):
-    regex_countdown = r"countdown"
-    if re.search(regex_countdown, command):
-        countdown = actions.countdown.Countdown()
-        print(countdown.getRemainingTime())
+    regex_countdown = r"countdown" #maybe (?i)countdown
+    if re.search(regex_countdown, command, re.IGNORECASE):
+        countdown = brain.actions.countdown.Countdown()
+        return countdown.getRemainingTime()
+    return "Invalid Command"
 
 
-
+def parse_message(message):
+    msg_str = message['command']
+    message['response_text'] = parse_command(msg_str)
+    return message
 
 def main():
     # my code here
-    parse_command('holacountdown')
+    return
 
 
 if __name__ == "__main__":
